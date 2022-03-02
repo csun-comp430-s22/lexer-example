7988499f11929184b6c8637b420eaacb44278514
Kyle Dewey <kyle.dewey@csun.edu>
2022-03-02 10:50:33 -0800

    added support for variables; untested

--- src/main/java/lexer/Tokenizer.java
+++ src/main/java/lexer/Tokenizer.java
@@ -45,49 +45,76 @@ public class Tokenizer {
         }
     }
 
-    public Token tryTokenizeVariable() {
+    // returns null if it fails to read in any variable or keyword
+    public Token tryTokenizeVariableOrKeyword() {
+        skipWhitespace();
+        
         String name = "";
 
         // idea: read one character at a time
         // when we are out of characters, check what the name is
         // if the name is special (e.g., "true"), emit the special token for it (e.g., TrueToken)
         // if the name isn't special (e.g., "foo"), emit a variable token for it (e.g., VariableToken("foo"))
-        
-        
-    // returns null if there are no more tokens left
-    public Token tokenizeSingle() throws TokenizerException {
-        skipWhitespace();
-        if (offset < input.length()) {
-            if (input.startsWith("true", offset)) {
-                offset += 4;
+        //
+        // First character of the variable: letter
+        // Every subsequent character: letter or a digit
+        //
+
+        if (offset < input.length() &&
+            Character.isLetter(input.charAt(offset))) {
+            name += input.charAt(offset);
+            offset++;
+
+            while (offset < input.length() &&
+                   Character.isLetterOrDigit(input.charAt(offset))) {
+                name += input.charAt(offset);
+                offset++;
+            }
+
+            // by this point, `name` holds a potential variable
+            // `name` could be "true"
+            if (name.equals("true")) {
                 return new TrueToken();
-            } else if (input.startsWith("false", offset)) {
-                offset += 5;
+            } else if (name.equals("false")) {
                 return new FalseToken();
-            } else if (input.startsWith("if", offset)) {
-                offset += 2;
+            } else if (name.equals("if")) {
                 return new IfToken();
-            } else if (input.startsWith("(", offset)) {
-                offset += 1;
-                return new LeftParenToken();
-            } else if (input.startsWith(")", offset)) {
-                offset += 1;
-                return new RightParenToken();
-            }  else if (input.startsWith("{", offset)) {
-                offset += 1;
-                return new LeftCurlyToken();
-            } else if (input.startsWith("}", offset)) {
-                offset += 1;
-                return new RightCurlyToken();
-            } else if (input.startsWith("else", offset)) {
-                offset += 4;
+            } else if (name.equals("else")) {
                 return new ElseToken();
             } else {
-                throw new TokenizerException();
+                return new VariableToken(name);
             }
         } else {
             return null;
         }
+    }
+    
+    // returns null if there are no more tokens left
+    public Token tokenizeSingle() throws TokenizerException {
+        Token retval = null;
+        skipWhitespace();
+        if (offset < input.length()) {
+            retval = tryTokenizeVariableOrKeyword();
+            if (retval == null) {
+                if (input.startsWith("(", offset)) {
+                    offset += 1;
+                    retval = new LeftParenToken();
+                } else if (input.startsWith(")", offset)) {
+                    offset += 1;
+                    retval = new RightParenToken();
+                }  else if (input.startsWith("{", offset)) {
+                    offset += 1;
+                    retval = new LeftCurlyToken();
+                } else if (input.startsWith("}", offset)) {
+                    offset += 1;
+                    retval = new RightCurlyToken();
+                } else {
+                    throw new TokenizerException();
+                }
+            }
+        }
+
+        return retval;
     }
     
     public List<Token> tokenize() throws TokenizerException {
